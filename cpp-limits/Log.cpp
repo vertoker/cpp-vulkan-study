5df3613be1c6d337fd95cbdabf2a86a4b7a203bd
vertoker <vertogpro@gmail.com>
2024-07-04 16:21:08 +0500

    define nameof functions like in c sharp

--- cpp-limits/Log.cpp
+++ cpp-limits/Log.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#define nameof(v) #v
+
 void Log(const char* message)
 #include "StartBrace.h"
 	std::cout << message << std::endl;
 #include "EndBrace.h"
+
+void Nameof() {
+	Log(nameof(Log));
+}
--- cpp-limits/Log.h
+++ cpp-limits/Log.h
@@ -1,1 +1,4 @@
+
 void Log(const char* message);
+
+void Nameof();
